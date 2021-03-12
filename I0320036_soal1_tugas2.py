1c0d64dc94f14546a312b48e3d1280667a5e092c
Erysa Putri <erysaaputri@gmail.com>
2021-03-13 01:54:17 +0700

    Soal 1 Tugas 2

---
+++ I0320036_soal1_tugas2.py
@@ -0,0 +1,51 @@
+#Menghitung Luas Persegi Panjang
+print ("Hai, kamu yang disana!<3")
+print ("˗ˏˋ Menghitung Luas Persegi Panjang ´ˎ˗")
+print ("Ayo masukkan panjang dan lebar dari persegi panjang!")
+Panjang = float(input("Panjang (meter) : "))
+Lebar = float(input("Lebar (meter) : "))
+#Rumus luas persegi panjang
+Luas = Panjang*Lebar
+print ("°•*⁀➷ Luas dari persegi panjang yang kamu miliki adalah", Luas, "m^2")
+print ("Sampai jumpa lagi!",'\n')
+
+#Menghitung Luas Lingkaran
+print ("Hai, kamu yang disana!<3")
+print ("˗ˏˋ Menghitung Luas Lingkaran ´ˎ˗")
+print ("Ayo masukkan jari-jari lingkaran!")
+Phi = 3.14
+R_Lingkaran = float(input("Jari-jari (meter) : "))
+#Rumus luas lingkaran
+Luas = (Phi*R_Lingkaran**2)
+print ("°•*⁀➷ Luas dari lingkaran yang kamu miliki adalah", Luas, "m^2")
+print ("Sampai jumpa lagi!",'\n')
+
+#Menghitung Luas Permukaan Kubus
+print ("Hai, kamu yang disana!<3")
+print ("˗ˏˋ Menghitung Luas Permukaan Kubus ´ˎ˗")
+print ("Ayo masukkan panjang sisi dari kubus!")
+Sisi = float(input("Sisi (meter) : "))
+#Rumus luas permukaan kubus
+Luas = (6*Sisi**2)
+print ("°•*⁀➷ Luas permukaan dari kubus yang kamu miliki adalah", Luas, "m^2")
+print ("Sampai jumpa lagi!",'\n')
+
+#Mengkonversi Celsius ke Fahrenheit
+print ("Hai, kamu yang disana!<3")
+print ("˗ˏˋ Mengkonversi Nilai Suhu Celsius ke Fahrenheit ´ˎ˗")
+print ("Ayo masukkan derajat suhu dalam celsius!")
+Celsius = float(input("Suhu Celsius : "))
+#Rumus konversi
+Konversi = (9/5*Celsius+32)
+print ("°•*⁀➷ Hasil dari konversi suhu adalah", Konversi,"°F")
+print ("Sampai jumpa lagi!",'\n')
+
+#Mengkonversi Reamur ke Kelvin
+print ("Hai, kamu yang disana!<3")
+print ("˗ˏˋ Mengkonversi Nilai Suhu Reamur ke Kelvin ´ˎ˗")
+print ("Ayo masukkan derajat suhu dalam reamur!")
+Reamur = float(input("Suhu Celsius : "))
+#Rumus konversi
+Konversi = (5/4*Reamur+273)
+print ("°•*⁀➷ Hasil dari konversi suhu adalah", Konversi,"K")
+print ("Sampai jumpa lagi!")
